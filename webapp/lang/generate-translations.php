6139f5efbe797bebc026bcf0fe31347e3b22654c
Leonel Dias <leonel.j.dias@gmail.com>
2025-12-07 19:19:43 +0000

    Update generate-translations.php

--- webapp/lang/generate-translations.php
+++ webapp/lang/generate-translations.php
@@ -3,7 +3,7 @@
 /**
  * Script to convert Laravel PHP translation files to JSON for Vue i18n
  */
-$locales = ['en', 'pt'];
+$langDir = __DIR__ . '/../lang';
 $outputDir = __DIR__ . '/../resources/js/translations';
 
 // Create output directory if it doesn't exist
@@ -11,34 +11,28 @@ if (!is_dir($outputDir)) {
     mkdir($outputDir, 0755, true);
 }
 
+// Read all folders inside lang (each folder is a locale)
+$locales = array_filter(scandir($langDir), function($dir) use ($langDir) {
+    return $dir !== '.' && $dir !== '..' && is_dir($langDir . '/' . $dir);
+});
+
 foreach ($locales as $locale) {
-    $langPath = __DIR__ . '/../lang/' . $locale;
-
-    if (!is_dir($langPath)) {
-        echo "Warning: Language directory not found: $langPath\n";
-
-        continue;
-    }
-
     $translations = [];
+    $langPath = $langDir . '/' . $locale;
 
-    // Get all PHP files in the lang directory
+    // Read all PHP files in the folder
     $files = glob($langPath . '/*.php');
-
     foreach ($files as $file) {
         $filename = basename($file, '.php');
         $content = require $file;
-
         if (is_array($content)) {
             $translations[$filename] = $content;
         }
     }
 
-    // Convert to JSON and save
+    // Save as JSON
     $jsonFile = $outputDir . '/' . $locale . '.json';
-    $json = json_encode($translations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
-    file_put_contents($jsonFile, $json);
+    file_put_contents($jsonFile, json_encode($translations, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
     echo "Generated: $jsonFile\n";
 }
